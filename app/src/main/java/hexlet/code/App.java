1efb6332726d60091f962711425e110242349276
serebrianskii <serebrianskiik@gmail.com>
2023-09-05 12:25:11 +0300

    Implement picocli skeleton

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,18 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+@Command(name = "gendiff", version = "1.0", mixinStandardHelpOptions = true)
+public class App implements Runnable {
     public static void main(String[] args) {
-        System.out.println("Test project init");
+        App app = new App();
+        new CommandLine(app).execute(args);
     }
+
+    public void run() {
+        var diff = Differ.generate("str1", "str2");
+        System.out.println(diff);
+    }
+
 }
