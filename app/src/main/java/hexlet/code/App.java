570075b56e28946d358556a852154d07f4b453d2
serebrianskii <serebrianskiik@gmail.com>
2023-09-05 17:34:12 +0300

    Add parameters and --format option prototypes to the App

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,16 +2,27 @@ package hexlet.code;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
-@Command(name = "gendiff", version = "1.0", mixinStandardHelpOptions = true)
+@Command(name = "gendiff", version = "0.0.1", mixinStandardHelpOptions = true)
 public class App implements Runnable {
     public static void main(String[] args) {
         App app = new App();
         new CommandLine(app).execute(args);
     }
 
+    @Parameters(index = "0", description = "path to first file", paramLabel = "filepath1")
+    private String filepath1;
+
+    @Parameters(index = "1", description = "path to second file", paramLabel = "filepath2")
+    private String filepath2;
+
+    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]", paramLabel = "format", defaultValue = "stylish")
+    private String format;
+
     public void run() {
-        var diff = Differ.generate("str1", "str2");
+        var diff = Differ.generate(filepath1, filepath2);
         System.out.println(diff);
     }
 
