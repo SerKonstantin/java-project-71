b36d26a58e235da555d36a3560fcf9f947b9b923
serebrianskii <serebrianskiik@gmail.com>
2023-09-14 16:38:28 +0300

    Change tests to include nested objects and null values

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -13,27 +13,26 @@ class DifferTest {
         String actual = Differ.generate(path1, path2);
         String expected = """
                 {
-                  - follow: false
-                    host: hexlet.io
-                  - proxy: 123.234.53.22
-                  - timeout: 50
-                  + timeout: 20
-                  + verbose: true
-                }""";
-        assertEquals(actual, expected);
-    }
-
-    @Test
-    public void generatePartlyEmptyTestJson() {
-        String path1 = "src/test/resources/testData0.json";
-        String path2 = "src/test/resources/testData2.json";
-        String actual = Differ.generate(path1, path2);
-        String expected = """
-                {
-                  - follow: false
-                  - host: hexlet.io
-                  - proxy: 123.234.53.22
-                  - timeout: 50
+                    bool1: true
+                  - bool2: false
+                  + bool2: Now I'm a string
+                    chars1: [a, b, c]
+                  - chars2: [d, e, f]
+                  + chars2: [m, m, m]
+                  + completely new string value: value2
+                  - int to null: 45
+                  + int to null: null
+                  + newObj: {nestedKey=value, isNested=true}
+                    null to null: null
+                  - null to string: null
+                  + null to string: Some string
+                  - numbers: [5, 6, 7, 8]
+                  + numbers: [4, 8, 15, 16]
+                    obj1: {nestedKey=value, isNested=true}
+                  - string to get rid off: value1
+                    string1: I won't change
+                  - string2: I'll change
+                  + string2: I changed
                 }""";
         assertEquals(actual, expected);
     }
@@ -62,12 +61,26 @@ class DifferTest {
         String actual = Differ.generate(path1, path2);
         String expected = """
                 {
-                  - follow: false
-                    host: hexlet.io
-                  - proxy: 123.234.53.22
-                  - timeout: 50
-                  + timeout: 20
-                  + verbose: true
+                    bool1: true
+                  - bool2: false
+                  + bool2: Now I'm a string
+                    chars1: [a, b, c]
+                  - chars2: [d, e, f]
+                  + chars2: [m, m, m]
+                  + completely new string value: value2
+                  - int to null: 45
+                  + int to null: null
+                  + newObj: {nestedKey=value, isNested=true}
+                    null to null: null
+                  - null to string: null
+                  + null to string: Some string
+                  - numbers: [5, 6, 7, 8]
+                  + numbers: [4, 8, 15, 16]
+                    obj1: {nestedKey=value, isNested=true}
+                  - string to get rid off: value1
+                    string1: I won't change
+                  - string2: I'll change
+                  + string2: I changed
                 }""";
         assertEquals(actual, expected);
     }
--- app/src/test/java/hexlet/code/ParserTest.java
+++ app/src/test/java/hexlet/code/ParserTest.java
@@ -2,6 +2,8 @@ package hexlet.code;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -12,24 +14,44 @@ class ParserTest {
     @Test
     public void parseJsonTest() {
         var actual = Parser.parseJson("src/test/resources/testData0.json");
-        var expected = Map.of(
-                 "host", "hexlet.io",
-                 "timeout", 50,
-                 "proxy", "123.234.53.22",
-                 "follow", false
-         );
+
+        Map<String, Object> expected = new HashMap<>();
+        // Looks redundant, but Map.of() doesn't allow more than 10 arguments
+        expected.put("string1", "I won't change");
+        expected.put("string2", "I'll change");
+        expected.put("string to get rid off", "value1");
+        expected.put("bool1", true);
+        expected.put("bool2", false);
+        expected.put("numbers", List.of(5, 6, 7, 8));
+        expected.put("chars1", List.of("a", "b", "c"));
+        expected.put("chars2", List.of("d", "e", "f"));
+        expected.put("int to null", 45);
+        expected.put("null to string", null);
+        expected.put("null to null", null);
+        expected.put("obj1", Map.of("nestedKey", "value", "isNested", true));
+
         assertEquals(actual, expected);
     }
 
     @Test
     public void parseYmlTest() {
         var actual = Parser.parseYml("src/test/resources/testData0.yml");
-        var expected = Map.of(
-                "host", "hexlet.io",
-                "timeout", 50,
-                "proxy", "123.234.53.22",
-                "follow", false
-        );
+
+        Map<String, Object> expected = new HashMap<>();
+        // Looks redundant, but Map.of() doesn't allow more than 10 arguments
+        expected.put("string1", "I won't change");
+        expected.put("string2", "I'll change");
+        expected.put("string to get rid off", "value1");
+        expected.put("bool1", true);
+        expected.put("bool2", false);
+        expected.put("numbers", List.of(5, 6, 7, 8));
+        expected.put("chars1", List.of("a", "b", "c"));
+        expected.put("chars2", List.of("d", "e", "f"));
+        expected.put("int to null", 45);
+        expected.put("null to string", null);
+        expected.put("null to null", null);
+        expected.put("obj1", Map.of("nestedKey", "value", "isNested", true));
+
         assertEquals(actual, expected);
     }
 
