e10cf3fa11beba99a1a93d06756aa54c1bb39779
serebrianskii <serebrianskiik@gmail.com>
2023-09-15 15:45:05 +0300

    Refactor Formatter class

--- app/src/main/java/hexlet/code/Formatter.java
+++ app/src/main/java/hexlet/code/Formatter.java
@@ -1,54 +1,17 @@
 package hexlet.code;
 
+import hexlet.code.formatters.Stylish;
+
 import java.util.List;
 
 public class Formatter {
     public static String generate(List<List<Object>> data, String format) {
         switch (format) {
             case "stylish":
-                return generateStylish(data);
+                return Stylish.generate(data);
+
             default:
                 throw new RuntimeException("Invalid format");
         }
-    }
-
-    public static String generateStylish(List<List<Object>> data) {
-        StringBuilder sb = new StringBuilder("{\n");
-
-        for (var dataLine: data) {
-            if (dataLine.get(0).equals("added")) {
-                sb.append("  + ")
-                        .append(dataLine.get(1))
-                        .append(": ")
-                        .append(dataLine.get(3))
-                        .append("\n");
-            } else if (dataLine.get(0).equals("removed")) {
-                sb.append("  - ")
-                        .append(dataLine.get(1))
-                        .append(": ")
-                        .append(dataLine.get(2))
-                        .append("\n");
-            } else if (dataLine.get(0).equals("unchanged")) {
-                sb.append("    ")
-                        .append(dataLine.get(1))
-                        .append(": ")
-                        .append(dataLine.get(2))
-                        .append("\n");
-            } else {
-                sb.append("  - ")
-                        .append(dataLine.get(1))
-                        .append(": ")
-                        .append(dataLine.get(2))
-                        .append("\n")
-                        .append("  + ")
-                        .append(dataLine.get(1))
-                        .append(": ")
-                        .append(dataLine.get(3))
-                        .append("\n");
-            }
-        }
-        sb.append("}");
-
-        return sb.toString();
     }
 }
