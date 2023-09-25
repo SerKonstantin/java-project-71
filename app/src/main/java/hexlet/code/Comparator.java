e360286b369b1773f8d98dc13a00d0889c7674ac
serebrianskii <serebrianskiik@gmail.com>
2023-09-26 00:16:26 +0300

    Refactor Comparator class
    In the previous version of the Comparator class, the returned List included lines of data where both value1 from the first file and value2 from the second file were presented, even if there was no such value in the corresponding file (in that case, value == null).
    
    Now the returned List only contains lines with usable data. If a file doesn't contain a specific value, it won't appear in the line of data.

--- app/src/main/java/hexlet/code/Comparator.java
+++ app/src/main/java/hexlet/code/Comparator.java
@@ -11,34 +11,33 @@ import static java.util.Objects.deepEquals;
 
 public class Comparator {
     public static List<Map<String, ?>> compareMaps(Map<String, ?> data1, Map<String, ?> data2) {
-        SortedSet<String> combinedKeys = new TreeSet<>(data1.keySet());
+        SortedSet<String> combinedKeys = new TreeSet<>();
+        combinedKeys.addAll(data1.keySet());
         combinedKeys.addAll(data2.keySet());
 
         List<Map<String, ?>> diffData = new ArrayList<>();
+
         for (var key: combinedKeys) {
-            String status;
+            var diffLine = new HashMap<String, Object>();
+            diffLine.put("key", key);
             if (!data1.containsKey(key)) {
-                status = "added";
+                diffLine.put("status", "added");
+                diffLine.put("value2", data2.get(key));
             } else if (!data2.containsKey(key)) {
-                status = "removed";
+                diffLine.put("status", "removed");
+                diffLine.put("value1", data1.get(key));
             } else if (deepEquals(data1.get(key), (data2.get(key)))) {
-                status = "unchanged";
+                diffLine.put("status", "unchanged");
+                diffLine.put("value1", data1.get(key));
             } else {
-                status = "updated";
+                diffLine.put("status", "updated");
+                diffLine.put("value1", data1.get(key));
+                diffLine.put("value2", data2.get(key));
             }
 
-            diffData.add(createDiffLine(status, key, data1.get(key), data2.get(key)));
+            diffData.add(diffLine);
         }
 
         return diffData;
-    }
-
-    public static Map<String, ?> createDiffLine(String status, String key, Object value1, Object value2) {
-        var diffLine = new HashMap<String, Object>();
-        diffLine.put("status", status);
-        diffLine.put("key", key);
-        diffLine.put("value1", value1);
-        diffLine.put("value2", value2);
-        return diffLine;
     }
 }
